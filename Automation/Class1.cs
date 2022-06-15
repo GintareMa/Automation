179a8b0a55f26fd74ad2eaf798ee93fa5008565d
gintare.mazeikyte <gintare.mazeikyte@zenitech.co.uk>
2022-06-15 23:00:40 +0300

    new test namu darbai parseUserAgent

--- Automation/Class1.cs
+++ Automation/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,75 @@ namespace Automation
 {
     public class Class1
     {
+        [Test]
+
+        public static void number4IsEven()
+        {
+            int expectedRemainder = 0;
+            int actualRemainder = 4 % 2;
+
+            Assert.AreEqual(expectedRemainder, actualRemainder);
+            //Assert.IsTrue(expectedRemainder == actualRemainder);
+        }
+
+        [Test]
+
+        public static void currenHourIsEqualTo19()
+        {
+            int expectedHout = 19;
+            int actualHour = DateTime.Now.Hour;
+
+            Assert.AreEqual(expectedHout, actualHour);
+        }
+        [Test]
+
+        public static void number995DividesBy3WithoutRemainder()
+        {
+            int expectedRemainder = 0;
+            int actualRemainder = 995 % 3;
+
+            Assert.AreEqual(expectedRemainder, actualRemainder);
+        }
+
+        [Test]
+
+        public static void treciadienis()
+        {
+            //   int expectedDay = 3;
+            //   int actualDay = (int)DateTime.Now.DayOfWeek;
+
+            //  Assert.AreEqual(expectedDay, actualDay);
+
+
+            DayOfWeek expectedDay = DayOfWeek.Wednesday;
+            DayOfWeek actualDay = DateTime.Now.DayOfWeek;
+
+            Assert.AreEqual(expectedDay, actualDay);
+        }
+
+        [Test]
+
+        public static void waitFor5Seconds()
+        {
+            System.Threading.Thread.Sleep(5000);          
+        }
+
+        [Test]
+
+        public static void thereAre4EventNumbersBetween1And10()
+        {
+            int expectedCount = 4;
+            int actualCount = 0;
+
+            for (int number = 1; number < 10; number++)
+            {
+                if (number % 2 == 0)
+                {
+                    actualCount++;
+                } 
+            }
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
     }
 }
---
+++ Automation/Class2.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Automation
+{
+    internal class Class2
+    {
+        [Test]
+
+        public static void openDemoQA()
+        {
+            IWebDriver driver = new ChromeDriver();
+              driver.Url = "https://demoqa.com/text-box";
+
+              IWebElement inputFullName1 = driver.FindElement(By.Id("userName"));
+              IWebElement inputFullName2 = driver.FindElement(By.XPath("//*[@id='userName']"));
+              inputFullName2.SendKeys("Gintare");
+
+              string actualInput = inputFullName2.Text;
+            
+        }
+
+        [Test]
+
+        public static void parseUserAgent()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://developers.whatismybrowser.com/";
+
+            driver.FindElement(By.XPath("//*[@type='submit']")).Click();
+
+            string parsedText = driver.FindElement(By.XPath("//*[@class='simple-major']")).Text;
+            string actualText = "Chrome 102 on Windows 10";
+
+            Assert.AreEqual(parsedText, actualText);
+
+            driver.Close();
+
+        }
+    }
+
+}
